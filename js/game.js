2e6646c1334f4444711a626bbc73b47c8ec55aea
Sherwin <sherwino@outlook.com>
2022-09-21 16:57:43 -0400

    fix: check game status

--- js/game.js
+++ js/game.js
@@ -1,69 +1,65 @@
 //------------------------------------------------
 
-
-function Game2048 () {
+function Game2048() {
   this.board = [
-    [null,null,null,null],
-    [null,null,null,null],
-    [null,null,null,null],
-    [null,null,null,null]
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
+    [null, null, null, null],
   ];
 
-  this.score  = 0;
-  this.won   = false;
-  this.lost  = false;
+  this.score = 0;
+  this.won = false;
+  this.lost = false;
   this._generateTile();
   this._generateTile();
 }
 
 Game2048.prototype._generateTile = function () {
   //if condition = true ? return x : return y;
-  var initialValue = (Math.random() < 0.8 ? 2:4);
+  var initialValue = Math.random() < 0.8 ? 2 : 4;
 
   var emptyTile = this._getAvailablePosition();
   //if we have an empty tile fill it in with the found position
   if (emptyTile) {
     this.board[emptyTile.x][emptyTile.y] = initialValue;
   }
-
 };
 Game2048.prototype._getAvailablePosition = function () {
-
   //if we have an empty tile then update board with the found position
   var emptyTiles = [];
 
-  this.board.forEach(function(row, rowIndex){
-    row.forEach(function(elem, colIndex){
+  this.board.forEach(function (row, rowIndex) {
+    row.forEach(function (elem, colIndex) {
       if (!elem) emptyTiles.push({ x: rowIndex, y: colIndex });
     });
   });
 
-  if (emptyTiles.length === 0)
-    return false;
+  if (emptyTiles.length === 0) return false;
 
   var randomPosition = Math.floor(Math.random() * emptyTiles.length);
   return emptyTiles[randomPosition];
 };
-
 Game2048.prototype._renderBoard = function () {
-  this.board.forEach(function(row){ console.log(row); });
-  console.log('Score: ' + this.score);
+  this.board.forEach(function (row) {
+    console.log(row);
+  });
+  console.log("Score: " + this.score);
 };
-
 Game2048.prototype._moveLeft = function () {
   var newBoard = [];
   var that = this;
   var boardChanged = false;
 
-  this.board.forEach (function (row) {
+  this.board.forEach(function (row) {
     var newRow = row.filter(function (i) {
       return i !== null;
     });
 
-    for(i = 0; i < newRow.length - 1; i++) {
-      if (newRow[i+1] === newRow[i]) {
-        newRow[i]   = newRow[i] * 2;
-        newRow[i+1] = null;
+    for (i = 0; i < newRow.length - 1; i++) {
+      if (newRow[i + 1] === newRow[i]) {
+        newRow[i] = newRow[i] * 2;
+        newRow[i + 1] = null;
       }
     }
 
@@ -71,12 +67,11 @@ Game2048.prototype._moveLeft = function () {
       return i !== null;
     });
 
-    while(merged.length < 4) {
+    while (merged.length < 4) {
       merged.push(null);
     }
 
-    if (newRow.length !== row.length)
-      boardChanged = true;
+    if (newRow.length !== row.length) boardChanged = true;
 
     newBoard.push(merged);
   });
@@ -84,21 +79,20 @@ Game2048.prototype._moveLeft = function () {
   this.board = newBoard;
   return boardChanged;
 };
-
 Game2048.prototype._moveRight = function () {
   var newBoard = [];
   var that = this;
   var boardChanged = false;
 
-  this.board.forEach (function (row) {
+  this.board.forEach(function (row) {
     var newRow = row.filter(function (i) {
       return i !== null;
     });
     //set current index to the
-    for (i=newRow.length - 1; i>0; i--) {
-      if (newRow[i-1] === newRow[i]) {
-        newRow[i]   = newRow[i] * 2;
-        newRow[i-1] = null;
+    for (i = newRow.length - 1; i > 0; i--) {
+      if (newRow[i - 1] === newRow[i]) {
+        newRow[i] = newRow[i] * 2;
+        newRow[i - 1] = null;
         that._updateScore(newRow[i]);
       }
       //if the length of the new row does not equal current row length
@@ -109,7 +103,7 @@ Game2048.prototype._moveRight = function () {
       return i !== null;
     });
     //while merged length is less than 4
-    while(merged.length < 4) {
+    while (merged.length < 4) {
       //item to the front of the array
       merged.unshift(null);
     }
@@ -120,25 +114,22 @@ Game2048.prototype._moveRight = function () {
   this.board = newBoard;
   return boardChanged;
 };
-
 Game2048.prototype._moveUp = function () {
   this._transposeMatrix();
   var boardChanged = this._moveLeft();
   this._transposeMatrix();
   return boardChanged;
 };
-
 Game2048.prototype._moveDown = function () {
   this._transposeMatrix();
   var boardChanged = this._moveRight();
   this._transposeMatrix();
   return boardChanged;
 };
-
 // Transpose the 4x4 board...in other words turn it 90 deg
 Game2048.prototype._transposeMatrix = function () {
   for (var row = 0; row < this.board.length; row++) {
-    for (var column = row+1; column < this.board.length; column++) {
+    for (var column = row + 1; column < this.board.length; column++) {
       var temp = this.board[row][column];
       this.board[row][column] = this.board[column][row];
       this.board[column][row] = temp;
@@ -150,10 +141,18 @@ Game2048.prototype.move = function (direction) {
   ion.sound.play("beer_can_opening");
   if (!this._gameFinished()) {
     switch (direction) {
-      case "up":    boardChanged = this._moveUp();    break;
-      case "down":  boardChanged = this._moveDown();  break;
-      case "left":  boardChanged = this._moveLeft();  break;
-      case "right": boardChanged = this._moveRight(); break;
+      case "up":
+        boardChanged = this._moveUp();
+        break;
+      case "down":
+        boardChanged = this._moveDown();
+        break;
+      case "left":
+        boardChanged = this._moveLeft();
+        break;
+      case "right":
+        boardChanged = this._moveRight();
+        break;
     }
 
     if (boardChanged) {
@@ -162,7 +161,7 @@ Game2048.prototype.move = function (direction) {
   }
 };
 
-Game2048.prototype._updateScore = function(value) {
+Game2048.prototype._updateScore = function (value) {
   this.score += value;
 
   if (value === 2048) {
@@ -170,17 +169,15 @@ Game2048.prototype._updateScore = function(value) {
   }
 };
 
-
 Game2048.prototype._gameFinished = function () {
   return this.lost;
 };
 
 Game2048.prototype._isGameLost = function () {
-  if (this._getAvailablePosition())
-    return;
+  if (this._getAvailablePosition()) return;
 
-  var that   = this;
-  var isLost = true;
+  var that = this;
+  this.lost = true;
 
   this.board.forEach(function (row, rowIndex) {
     row.forEach(function (cell, cellIndex) {
@@ -188,24 +185,27 @@ Game2048.prototype._isGameLost = function () {
       var top, bottom, left, right;
 
       if (that.board[rowIndex][cellIndex - 1]) {
-        left  = that.board[rowIndex][cellIndex - 1];
+        left = that.board[rowIndex][cellIndex - 1];
       }
       if (that.board[rowIndex][cellIndex + 1]) {
         right = that.board[rowIndex][cellIndex + 1];
       }
       if (that.board[rowIndex - 1]) {
-        top    = that.board[rowIndex - 1][cellIndex];
+        top = that.board[rowIndex - 1][cellIndex];
       }
       if (that.board[rowIndex + 1]) {
         bottom = that.board[rowIndex + 1][cellIndex];
       }
 
-      if (current === top || current === bottom || current === left || current === right)
-        isLost = false;
+      if (
+        current === top ||
+        current === bottom ||
+        current === left ||
+        current === right
+      )
+        this.lost = false;
     });
   });
-
-  this.lost = isLost;
 };
 
 window.onload = function () {
@@ -213,16 +213,16 @@ window.onload = function () {
   renderTiles();
 };
 
-function renderTiles () {
-  game.board.forEach(function(row, rowIndex){
+function renderTiles() {
+  game.board.forEach(function (row, rowIndex) {
     row.forEach(function (cell, cellIndex) {
       if (cell) {
         var tileContainer = document.getElementById("tile-container");
-        var newTile       = document.createElement("div");
+        var newTile = document.createElement("div");
 
-        newTile.classList  = "tile val-" + cell;
+        newTile.classList = "tile val-" + cell;
         newTile.classList += " tile-position-" + rowIndex + "-" + cellIndex;
-        newTile.innerHTML  = (cell);
+        newTile.innerHTML = cell;
 
         tileContainer.appendChild(newTile);
       }
@@ -230,8 +230,8 @@ function renderTiles () {
   });
 }
 
-function updateScore () {
-  var score          = game.score;
+function updateScore() {
+  var score = game.score;
   var scoreContainer = document.getElementsByClassName("js-score");
 
   Array.prototype.slice.call(scoreContainer).forEach(function (span) {
@@ -239,68 +239,76 @@ function updateScore () {
   });
 }
 
-function gameStatus () {
+function gameStatus() {
+  const gameoverEl = document.getElementById("game-over");
   if (game.lost()) {
-    document.getElementById("game-over").classList = "show-won";
-  } else if (game.lose()) {
-    document.getElementById("game-over").classList = "show-lost";
+    gameoverEl.innerText = "GAME OVER!";
+    gameoverEl.style.display = "block";
+  } else if (game.won) {
+    gameoverEl.innerText = "YOU WIN!";
+    gameoverEl.style.display = "block";
   }
 }
 
-function moveListeners (event) {
+function moveListeners(event) {
   var keys = [37, 38, 39, 40];
 
-  if (keys.indexOf(event.keyCode) < 0)
-
-    return;
+  if (keys.indexOf(event.keyCode) < 0) return;
 
   switch (event.keyCode) {
-    case 37: game.move("left");  break;
-    case 38: game.move("up");    break;
-    case 39: game.move("right"); break;
-    case 40: game.move("down");  break;
+    case 37:
+      game.move("left");
+      break;
+    case 38:
+      game.move("up");
+      break;
+    case 39:
+      game.move("right");
+      break;
+    case 40:
+      game.move("down");
+      break;
   }
 
   resetTiles();
   renderTiles();
   updateScore();
+  gameStatus();
 }
 
-function resetTiles () {
-
+function resetTiles() {
   var tilesContainer = document.getElementById("tile-container");
   //grab all the tiles off the tiilesContainer
-  var tiles          = tilesContainer.getElementsByClassName("tile");
-//taking the slice method, and lending it to this node collection getElementsByClassName and creating arrays from it
+  var tiles = tilesContainer.getElementsByClassName("tile");
+  //taking the slice method, and lending it to this node collection getElementsByClassName and creating arrays from it
   Array.prototype.slice.call(tiles).forEach(function (tile) {
     tilesContainer.removeChild(tile);
   });
 
-
-// for(i = 0; i < newRow.length - 1; i++) {
-//   if (newRow[i+1] === newRow[i]) {
-//     ion.sound.play("tap");
-//     // more code
-//   }
+  // for(i = 0; i < newRow.length - 1; i++) {
+  //   if (newRow[i+1] === newRow[i]) {
+  //     ion.sound.play("tap");
+  //     // more code
+  //   }
 }
 
 document.addEventListener("keydown", moveListeners);
 
 //load sounds from ion sound library ion.sound.min.js
-function loadSounds () {
+function loadSounds() {
   ion.sound({
-    sounds: [{name: "beer_can_opening"}, {name: "tap"}],
+    sounds: [{ name: "beer_can_opening" }, { name: "tap" }],
 
     path: "./js/lib/ion.sound-3.0.7/sounds/",
     preload: true,
-    volume: 1.0
+    volume: 1.0,
   });
 }
 
-$(document).ready(function (){
+$(document).ready(function () {
   var game = new Game2048();
   console.log("Initial Board ");
-  loadSounds ();
+  loadSounds();
   // game._renderBoard();
   // game.move("up");
   // game._renderBoard();
@@ -312,5 +320,4 @@ $(document).ready(function (){
   // game._renderBoard();
   //
   // console.log("Game has been loaded");
-
 });
